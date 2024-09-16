8269a34e3638549acb5a800b0c5d0ac8ab82596c
moolenbeek <emoolenbeek@gmail.com>
2024-09-15 21:41:12 -0400

    setup project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import "@picocss/pico";
+</script>
+
+<div class="container">
+	<nav>
+		<ul>
+			<li>
+				<a href="/"> Website </a>
+			</li>
+		</ul>
+		<ul>
+			<form method="POST">
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+			</form>
+		</ul>
+	</nav>
+	<slot />
+</div>
