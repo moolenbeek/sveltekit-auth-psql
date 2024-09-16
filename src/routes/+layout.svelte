917d2314a57754ec524bc4d9f1b5b31cfaed2751
moolenbeek <emoolenbeek@gmail.com>
2024-09-15 23:07:23 -0400

    update nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 		<ul>
 			<form method="POST">
 				<li><a href="/">Home</a></li>
-				<li><a href="/about">About</a></li>
+				<li><a href="/register">Register</a></li>
 			</form>
 		</ul>
 	</nav>
