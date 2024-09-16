baab9bd62e7ff881ef5678a45d69dbf87adbf3dd
moolenbeek <emoolenbeek@gmail.com>
2024-09-16 08:12:09 -0400

    stop tracking

--- src/lib/database/migrations/0000_rapid_red_ghost.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" text PRIMARY KEY NOT NULL,
-	"email" text NOT NULL,
-	"name" text NOT NULL,
-	"password" text,
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
