1d087f45a03edfccb5cb50429bd0b2ac5f2ff403
moolenbeek <emoolenbeek@gmail.com>
2024-09-17 21:37:21 -0400

    handle register

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,23 @@
+import { createUser } from "$lib/database/router/users";
+import { fail, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+import { Argon2id } from "oslo/password";
+
+export const actions: Actions = {
+	default: async function ({ request }) {
+		let { name, email, password } = Object.fromEntries(await request.formData()) as Record<
+			string,
+			string
+		>;
+
+		password = await new Argon2id().hash(password);
+		const id = crypto.randomUUID();
+		try {
+			await createUser({ id, email, name, password });
+		} catch (err) {
+			console.error(err);
+			return fail(400, { message: "Could not register user" });
+		}
+		throw redirect(302, "/login");
+	}
+};
