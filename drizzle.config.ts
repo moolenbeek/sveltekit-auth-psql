7327a91624f020068685609bd6382d8ca88c82c5
moolenbeek <emoolenbeek@gmail.com>
2024-09-17 21:35:04 -0400

    fix config error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "drizzle-kit";
-
+const { DATABASE_URL } = process.env;
+if (!DATABASE_URL) {
+	throw new Error("No url");
+}
 export default {
 	out: "./src/lib/database/migrations",
 	schema: "./src/lib/database/drizzle-schemas.ts",
