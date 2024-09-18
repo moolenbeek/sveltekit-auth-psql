1c5a7bdf6f2f2fa8c5dc401228f5f1f9d40599b8
moolenbeek <emoolenbeek@gmail.com>
2024-09-18 07:43:32 -0400

    add sessions table to schema

--- src/lib/database/drizzle-schemas.ts
+++ src/lib/database/drizzle-schemas.ts
@@ -4,7 +4,20 @@ export const userTable = pgTable("users", {
 	id: text("id").notNull().primaryKey(),
 	email: text("email").notNull().unique(),
 	name: text("name").notNull(),
-	password: text("password")
+	password: text("password"),
+	token: text("token").unique()
+});
+
+export const sessionTable = pgTable("sessions", {
+	id: text("id").notNull().primaryKey(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date"
+	}).notNull()
 });
 
 export type User = typeof userTable.$inferInsert;
+export type Session = typeof sessionTable.$inferInsert;
