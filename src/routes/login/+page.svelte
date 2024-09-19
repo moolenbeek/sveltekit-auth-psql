12ff4b2d65715b55a9a0a6738818734952331c3e
moolenbeek <emoolenbeek@gmail.com>
2024-09-19 17:29:24 -0400

    add checkbox to show password

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,23 @@
+<script lang="ts">
+	let showPassword = false;
+
+	function togglePasswordVisibility() {
+		showPassword = !showPassword;
+	}
+</script>
+
 <form method="POST">
 	<h2>Login</h2>
 	<label for="email">Email</label>
 	<input type="email" id="email" name="email" required />
 
 	<label for="password">Password</label>
-	<input type="password" id="password" name="password" required />
+	<input type={showPassword ? "text" : "password"} id="password" name="password" required />
+
+	<label>
+		<input type="checkbox" on:change={togglePasswordVisibility} />
+		Show password
+	</label>
 
 	<button type="submit">Login</button>
 </form>
