0a5bfbf61c66b664a4f2db0f64405206063b13fd
moolenbeek <emoolenbeek@gmail.com>
2024-09-19 07:54:07 -0400

    display users name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  
+  export let data: PageData;
+</script>
+
 <h2>Home</h2>
+
+{#if data.user}
+  <p>Welcome, {data.user.name}!</p>
+{:else}
+  <p>Not logged in</p>
+{/if}
